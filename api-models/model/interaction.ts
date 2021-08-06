a7fc4e8982981ba559d234856b6cd90231de52ba
CotesaAdmin <infosistemas@grupotecopy.es>
2021-08-06 11:32:51 +0000

    API definition transferred by SwaggerHub

---
+++ api-models/model/interaction.ts
@@ -0,0 +1,67 @@
+/**
+ * Api CRM
+ * Especificaciónes de la API pública de CRM.  # Versión 2 parte pública.  ***  Los métodos estaran protegidos mediante el paso de un token válido.  Los métodos que puedan ser accedidos por aplicaciones de terceros estarán protegidos por el paso de una api key que tendrá definidos los permisos correspondientes.  Se eliminan las descriciones de cambios de versiones anteriores, se pueden consultar en las versiones previas   ## 0.3.0 ***  Situation *** - Se redefine la clase con los atributos _id y situacionType situacionType será un objeto del tipo SituacionType que definirá la situación  SituationType *** - Se define la clase SituationType y sus métodos  Interactions *** - Se elimina el método filter - En el método de añadir situación se envía y retorna la situación añadida , no la interacción completa (/interactions/{id}/situation)  CrmRequest *** - Se elimina el método filter - se deprecan los métodos get y post de notes - se depreca el método get de sitauciones de una solicitud '/crmrequests/situations/{crmRequestId}  CensusData - se modifican los tipos devueltos para ajustarlos a lo importado desde padrón  ## 0.3.1 ***  Integrations *** - Se define la clase de integraciones y los métodos crud - PTE PARAMETROS GET PARA UPDATE O DELETE O INSERT  Persons *** - Se definen las nuevas propiedades para soportar el catálogo de importaciones - Se define nuevo método en API para obtener los datos de una integración de una persona - Se establece la propiedad segmentation como un array de SegmentItem  Segments *** - Se redefine la clase segments, se añade la propiedad ruleSet que establecerá las reglas para añadir el segmento a una persona - Se defina la clase SegmentItem - Se definen los métodos de la api de segmentos  Datasources *** - Se añade la propiedad orderBy como array de campos de ordenación del resultado  WidgetTable *** -  Añade la clase WidgetTable para su uso en los métodos widget  ## 0.3.2 ***  Address *** - Se añaden los campos number, floor, door, ladder, km, municipality y forcedAddress. - Se elimina el campo addressLine2  Dashboards *** - Se definen los métodos crud de dashboards - se define el esquema de dashboard   UsersDashboards *** - Se definen los métodos crud de la configuracion de dashboards  - Se define el esquema de usersDashboards y DashboardConfiguration  Datasources *** - Se especifica el retorno correcto del método /datasource/collectionsfields/{collection} es un objeto en vez de un array  Widgets *** - Se renombra la propiedad organizationalUnit a organizationalUnits y se convierte en array - Se elimina la propiedad privateWidget ya que no es necesaria al tener un array de organizationalUnits.       WidgetChart *** - Se elimina la propiedad xAxisLabels pues se gestionan las etiquetas desde la serie  DataSerie *** - Se define la propiedad dataSourceFieldForLabel que indica la propiedad que contiene las etiquetas de la serie  Persons *** - Nuevo método/persons/forcedvias que retorna las vías forzadas en direcciones de personas - Nuevo esquema ForcedVia - Nuevas propiedades segmentationWhiteList y segmentationBlackList   Users *** - Se define al usuario el dashboard por defecto en la propiedad defaultDashboard. Deberá actualizarse cada vez que se actualice ese dashboard  InteractionTypes , CrmRequestTypes *** - Se define la propiedad segmentation que contiene los segmentos que genera la interacción o solicitud  ## 0.3.3 ***  users *** Se especifica el retorno del error 460 cuando no se puede procesar la petición por condiciones de la lógica de la aplicación. P. Ej. el usuario se asocia a una persona que ya tiene usuario asociado  ServiceResponseError *** Se redefine el esquema del error en las respuestas para alinearlo con back  Personas *** Documenta núevos métodos lock y unlock ya implementados  CensusViaType *** Se define el modelo  CensusEntity *** Se define el modelo  Addresses *** Se modifica el modelo    addressType -se renombra a viaType del tipo pasa a CensusViaType   addressLine1 -se renombra a via y se define como CensusEntity   city, province(state) y municipality pasan a ser de tipo CensusEntity   state - se renombra a province  Census Data *** europeanCommunity y pasan de boolean a numérico para alinearlos con lo recibido de padrón  ## 0.3.4 ***   Addresses ***   -  Se modifica el modelo. Se añaden las propiedades letter, block, portal de tipo string para alinear las direcciones con padrón  Persons ***   - /persons/forcedvias/ - Se modifica la respuesta y la forma de obtener las vías forzadas. Se obtienen por id de municipio y se retorna la vía con su código y el tipo de vía con su código   - Se elimina el esquema ForcedVia, ahora se retorna CensusViaViaType    ##0.3.5  ***  Users *** Se define el nuevo método put /users/{userId}/defaultdashboard mediante el cual se establece la propieda defaultDashboard con el dashboard recibido en el body  ##0.3.6 ***  Person *** Se añade la propiedad electronicOffice a la persona  AdminstrativeFile *** Define el esquema de expediente administrativo de experta importado desde ETL  ElectronicOffice *** Se define el esquema de oficina electrónica que contiene los datos de sede electrónica de la persona 
+ *
+ * OpenAPI spec version: 0.3.6-PRIVATE
+ * Contact: juancromero@grupotecopy.es
+ *
+ * NOTE: This class is auto generated by the swagger code generator program.
+ * https://github.com/swagger-api/swagger-codegen.git
+ * Do not edit the class manually.
+ */
+import { Comment } from './comment';
+import { InteractionType } from './interactionType';
+import { Situation } from './situation';
+import { UserBasic } from './userBasic';
+
+/**
+ * Acción que realiza una persona, componente o sistema relacionado con la iniciativa que no precisa de gestión
+ */
+export interface Interaction { 
+    /**
+     * Como object id tendrá el timestamp de creación
+     */
+    id: string;
+    userAgent: UserBasic;
+    /**
+     * Identificador único del sujeto en la iniciativa. Podrá ser organización, ciudadano, visitante, empresa, etc. No es un atributo requerido porque se podrán registrar interacciones anónimas
+     */
+    subjectId?: string;
+    /**
+     * ID del componente o aplicación que genera la interacción, en las interactivas será CRM y en el resto será una aplicación previamente identificada
+     */
+    sourceExternalAplicationId: string;
+    interactionType: InteractionType;
+    /**
+     * Dirección de la interacción persona->sistema o viceversa
+     */
+    interactionDirection: Interaction.InteractionDirectionEnum;
+    /**
+     * Detalle de la interacción
+     */
+    detail: string;
+    /**
+     * Situaciones de la interacción en el caso de interacciones serán creadas y cerradas nomalmente por el agente ya que en otro caso hay que generar una solicitud pero se deja abierto para poder tener tiempos de resolución, atención,...
+     */
+    situations: Array<Situation>;
+    /**
+     * Comentarios a la interacción
+     */
+    comments: Array<Comment>;
+    /**
+     * Fecha de creación en formato según RFC 3339, section 5.6
+     */
+    createDate: string;
+    /**
+     * Fecha de última modificación en formato según RFC 3339, section 5.6
+     */
+    lastModifiedDate: string;
+    customFormData: any;
+}
+export namespace Interaction {
+    export type InteractionDirectionEnum = 'inbound' | 'outbound';
+    export const InteractionDirectionEnum = {
+        Inbound: 'inbound' as InteractionDirectionEnum,
+        Outbound: 'outbound' as InteractionDirectionEnum
+    };
+}
